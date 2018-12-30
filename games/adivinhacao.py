c06cbf32b960e93e3c8cff9bd3fb5ffa4556a1f8
BrunoASN <bruno_freddy@hotmail.com>
2018-12-30 18:24:16 -0200

    Aula 5, pt1

--- games/adivinhacao.py
+++ games/adivinhacao.py
@@ -1,19 +1,22 @@
 print("Bem vindo ao jogo de adivinhação!")
 
 num_secreto = 42
-total_tentativas = 1
+total_tentativas = 3
 
-while (total_tentativas <= 3):
-    print("\nTentativa",total_tentativas," de 3")
-    chute = int(input("Digite o seu numero: "))
+for rodada in range(1, total_tentativas + 1):
+    print("\nTentativa", rodada, " de 3")
+    chute = int(input("Digite o seu numero entre 0 e 100: "))
+    if (chute < 1 or chute > 100):
+        print("Você digitou um valor invalido.")
+        continue
     print("Você digitou ", chute)
 
     if (num_secreto == chute):
         print("Você acertou!")
+        break
     else:
         if (chute > num_secreto):
             print("Você errou! O chute foi maior.")
         elif (chute < num_secreto):
             print("Você errou! O chute foi menor.")
-    total_tentativas = total_tentativas + 1
 print("Fim de jogo!")
