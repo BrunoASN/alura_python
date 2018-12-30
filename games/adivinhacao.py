a59466ed49c997795c234fcf71eb27ad4b7ebba8
BrunoASN <bruno_freddy@hotmail.com>
2018-12-30 13:15:44 -0200

    Aula 4

--- games/adivinhacao.py
+++ games/adivinhacao.py
@@ -1,17 +1,19 @@
 print("Bem vindo ao jogo de adivinhação!")
 
-num_secreto=42
+num_secreto = 42
+total_tentativas = 1
 
-chute = int(input("Digite o seu numero: "))
+while (total_tentativas <= 3):
+    print("\nTentativa",total_tentativas," de 3")
+    chute = int(input("Digite o seu numero: "))
+    print("Você digitou ", chute)
 
-print("Você digitou ", chute)
-
-if (num_secreto==chute):
-    print("Você acertou!")
-else:
-    if(chute>num_secreto):
-        print("Você errou! O chute foi maior.")
-    elif (chute < num_secreto):
-        print("Você errou! O chute foi menor.")
-
+    if (num_secreto == chute):
+        print("Você acertou!")
+    else:
+        if (chute > num_secreto):
+            print("Você errou! O chute foi maior.")
+        elif (chute < num_secreto):
+            print("Você errou! O chute foi menor.")
+    total_tentativas = total_tentativas + 1
 print("Fim de jogo!")
