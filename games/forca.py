25a94196164894951698f8126b05dfe363d89ec1
BrunoASN <bruno_freddy@hotmail.com>
2019-03-04 18:34:11 -0300

    ok

--- games/forca.py
+++ games/forca.py
@@ -1,8 +1,11 @@
+import random
+
 def jogar_forca():
     print("Bem vindo ao jogo de forca!")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    palavra_secreta = carrega_palavra()
+
+    letras_acertadas = ininializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -13,7 +16,7 @@ def jogar_forca():
     while (not enforcou and not acertou):
 
         chute = input("Qual letra? ")  # Retira os espaços
-        chute = chute.strip().upper() # unper retorna sempre em maisculas, para minusculas utilizar o lower
+        chute = chute.strip().upper()  # unper retorna sempre em maisculas, para minusculas utilizar o lower
 
         if (chute in palavra_secreta):
             index = 0
@@ -27,11 +30,26 @@ def jogar_forca():
         enforcou = erros == 6  # limita o numero de tentativas em 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-    if(acertou):
+    if (acertou):
         print("Parabéns você ganhou!")
     else:
         print("Você perdeu!")
     print("Fim de jogo")
+
+def carrega_palavra():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def ininializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 
 if (__name__ == "__main__"):
